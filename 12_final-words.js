f541f663412d85e8bfd460256e928d4e243776d5
carlos121493 <guowei@shnow.cn>
2016-04-17 14:50:55 +0800

    完成12节最终致辞翻译

---
+++ 12_final-words.js
@@ -0,0 +1,14 @@
+// 第12节 最后致辞
+
+// 实际上任然有比我们教程中展示你更多关于Redux和react-redux的知识。
+// 举例来说，concerning Redux，你可能对bindActionCreators感兴趣（制作一个action creators的字典）
+// 已经被绑定了dispatch- http://rackt.org/redux/docs/api/bindActionCreators.html).
+
+// 我们希望能给你更清晰了解Flux的钥匙。
+// Flux应用和其他使用方式的不同，特别是Redux是如何表明的。
+
+// 我们接下来可以去哪里？
+
+// 官方Redux文档已经非常赞和全了，所以你现在不应该讨厌开始看他http://rackt.org/redux/index.html
+
+// 享受React和Redux的学习。
